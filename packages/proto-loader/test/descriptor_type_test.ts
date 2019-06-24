ec48aadf26dc87896ddd655cefb404ebcbb2a310
Tom Kirkpatrick <tkp@kirkdesigns.co.uk>
2019-06-24 14:35:57 +0200

    Revert "proto-loader: include Google well known protos"
    This reverts commit cbff0b46012ce286e1b54c7a05128667b0d72aad.

--- packages/proto-loader/test/descriptor_type_test.ts
+++ packages/proto-loader/test/descriptor_type_test.ts
@@ -94,9 +94,4 @@ describe('Descriptor types', () => {
       }
     );
   });
-
-  it('Can use well known Google protos', () => {
-    // This will throw if the well known protos are not available.
-    proto_loader.loadSync(`${TEST_PROTO_DIR}/well_known.proto`);
-  });
 });
